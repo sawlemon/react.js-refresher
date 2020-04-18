ede86fa6860b985068ca3d64eccf5cbc2e5dab9b
Sala-Khaliff-Jr <salakhaliff@gmail.com>
2020-04-18 18:17:47 +0530

    caution list

---
+++ src/components/CautionList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CautionList = () => {
+    return( 
+    <div>
+
+        <ul className="caution-list">
+            <li>Wash Hands</li>
+            <li>Don't go out</li>
+            <li>don't touch face</li>
+        </ul>
+    </div>
+    );
+};
+
+export default CautionList;
